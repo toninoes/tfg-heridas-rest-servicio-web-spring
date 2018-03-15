b542e7e6ea222bd7f807d6768219194b7c6b7a34
Antonio Ruiz Rondán <toninoes@gmail.com>
2018-03-15 19:48:16 +0100

    Primera prueba

---
+++ src/main/java/prueba/Application.java
@@ -0,0 +1,14 @@
+package prueba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+//public class Application {
+public class Application extends SpringBootServletInitializer {
+	
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(Application.class, args);
+	}
+}
